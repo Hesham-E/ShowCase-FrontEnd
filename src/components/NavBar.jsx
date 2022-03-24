23c5f7977cf5b3485e8a67923a75a83f5a28083c
unknown <hesham.elkaliouby@ucalgary.ca>
2022-03-23 19:51:08 -0600

    Formatting

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import "./NavBar.css";
 import "./GeneralStyles.css";
 
-const NavBar = (props) => { 
+const NavBar = (props) => {
     return (
         <div class="topnav">
-                <a class="active" href="/">Home</a>
-                <a href="/login">Sign-in</a>
-                <a href="/signup">Sign-up</a>
-                <div class="search-container">
-                    <form action="/searchresults">
-                        <input type="text" placeholder="Search.." name="search"/>
-                        <button type="submit">Search</button>
-                    </form>
-                </div>
+            <a class="active" href="/">Home</a>
+            <a href="/login">Sign-in</a>
+            <a href="/signup">Sign-up</a>
+            <div class="search-container">
+                <form action="/searchresults">
+                    <input type="text" placeholder="Search.." name="search" />
+                    <button type="submit">Search</button>
+                </form>
             </div>
+        </div>
     );
 };
 
