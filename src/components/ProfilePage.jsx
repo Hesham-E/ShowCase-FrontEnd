9e5c8cc53e215c714ba638e458d2936030c0b783
alliana <alliana.dp@gmail.com>
2022-03-25 16:48:35 -0600

    Added add post button

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -5,11 +5,19 @@ import PostCard from './PostCard';
 
 const ProfilePage = (props) => {
     let profilePic = require("../images/DefaultProfilePicture.png");
+    let addPostIcon = require("../images/addIcon.png");
+    let editPostIcon = require("../images/pencilIcon.png");
     let userName = props.user.First_Name.concat(" ", props.user.Last_Name);
     let userBio = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?";
 
     return (
         <div className="ProfilePage">
+            <div className="Icon">
+                <a href="/addpost">
+                    <img className="Icon" src={addPostIcon} alt="" />
+                </a>
+                <img className="Icon" src={editPostIcon} alt="" />
+            </div>
             <img className="ProfilePic" src={profilePic} alt="" />
             <hr className='HeaderLine' />
             <span className='HeaderText'>{userName}</span>
