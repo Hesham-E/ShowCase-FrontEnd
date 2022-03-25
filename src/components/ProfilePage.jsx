9afeea5c381f405a8f422bed71f12cfe2246e43d
alliana <alliana.dp@gmail.com>
2022-03-25 17:51:40 -0600

    edit profile button works

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router';
 
 const ProfilePage = (props) => {
 
-
     let profilePic = require("../images/DefaultProfilePicture.png");
     let addPostIcon = require("../images/addIcon.png");
     let editPostIcon = require("../images/pencilIcon.png");
@@ -14,20 +13,17 @@ const ProfilePage = (props) => {
     let userBio = props.profile.Biography;
     let navigate = useNavigate();
 
-    const editProfile = () => {
-        navigate("/edit-profile")
-    }
-
     return (
         <div className="ProfilePage">
             <div className="Icon">
                 <a href="/addpost">
                     <img className="Icon" src={addPostIcon} alt="" />
                 </a>
-                <img className="Icon" src={editPostIcon} alt="" />
+                < a href="/edit-profile">
+                    <img className="Icon" src={editPostIcon} alt="" />
+                </a>
             </div>
             <img className="ProfilePic" src={profilePic} alt="" />
-            {props.auth ? <button className='EditButton' onClick={editProfile}>Edit Profile</button> : {}}
             <hr className='HeaderLine' />
             <span className='HeaderText'>{userName}</span>
             <span className='BodyText'>{userBio}</span>
